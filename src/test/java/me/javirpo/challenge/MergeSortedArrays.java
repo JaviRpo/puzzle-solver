978a046993d78824ee2d9ae624fd69ccb7c570a6
Javier <javier.restrepo.rendon@gmail.com>
2022-09-20 08:36:06 -0500

    Challenge

---
+++ src/test/java/me/javirpo/challenge/MergeSortedArrays.java
@@ -0,0 +1,41 @@
+package me.javirpo.challenge;
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {2, 3, 6, 7, 0, 0, 0, 0};
+        int[] arr2 = {1, 6, 9, 10};
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+        merge(arr1, arr2);
+        System.out.println(Arrays.toString(arr1));
+    }
+    public static void merge(int[] arr1, int[] arr2) {
+        int end = arr1.length - 1;
+        int len = arr2.length;
+
+        int i=len-1, j=len-1;
+        while (i>=0 && j>=0) {
+            if (arr1[i] > arr2[j]) {
+                arr1[end] = arr1[i];
+                i--;
+            } else {
+                arr1[end] = arr2[j];
+                j--;
+            }
+            end--;
+        }
+        while(i>=0) {
+            arr1[end] = arr1[i];
+            i--;
+            end--;
+        }
+        while(j>=0) {
+            arr1[end] = arr2[j];
+            j--;
+            end--;
+        }
+    }
+
+}
